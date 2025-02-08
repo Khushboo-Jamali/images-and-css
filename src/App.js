de10527c23125a879095a0cd5804b9e03531a2af
khushboo-jamali <jamalikhushboo@gmail.com>
2025-02-08 21:59:23 +0500

    Event handling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import natureImage from "./images/image3783.png";
 import Header from "./Header";
 
 function App() {
+  let displayData = () => {
+    alert("Welcome to our first event");
+  };
+  let addData = (a, b) => {
+    // console.log(a + b);
+    alert(a + b);
+    // document.write(addData);
+  };
+
   return (
     <div className="App">
       <Header />
       <img src={natureImage} />
+
       <h1 className="text-[9px] text-red-900 font-bold">
         Hello Welcome to the tailwind css project
       </h1>
+      <button className="btn btn-primary" onClick={displayData}>
+        Save
+      </button>
+      <br />
+      <br />
+
+      <button
+        className="btn btn-danger text-white"
+        onClick={() => addData(10, 20)}
+      >
+        Add Data
+      </button>
     </div>
   );
 }
